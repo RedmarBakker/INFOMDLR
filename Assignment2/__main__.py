7589cb84d235a162143261c35980cbe9272f9956
RedmarBakker <redmar.bakker@outlook.com>
2025-05-14 11:36:49 +0200

    gitignore data

--- Assignment2/__main__.py
+++ Assignment2/__main__.py
@@ -4,11 +4,21 @@
 # (d) If there is a significant difference in training and testing accuracies, what could be a possible reason? What are the alternative models or approaches you would select? Select one and implement to further improve your results. Justify your choice.
 
 
+import h5py
 
+def get_dataset_name(filename_with_dir):
+    filename_without_dir = filename_with_dir.split('/')[-1]
+    temp = filename_without_dir.split('.')[:-1]
+    dataset_name = ''.join(temp)
 
+    return dataset_name
 
+filename_path = "Intra/train/rest1059231.h5"
 
+with h5py.File(filename_path, 'r') as f:
+    dataset_name = get_dataset_name(filename_path)
+    matrix = f.get(dataset_name)[()]
 
-
-
+    print(type(matrix))
+    print(matrix.shape)
 
