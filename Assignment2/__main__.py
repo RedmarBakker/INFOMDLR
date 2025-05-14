2cec26ffc267e7b56cf6e384de90e11349922fd4
RedmarBakker <redmar.bakker@outlook.com>
2025-05-14 10:59:34 +0200

    added assignment 2 structure

---
+++ Assignment2/__main__.py
@@ -0,0 +1,14 @@
+# (a) Choose a suitable deep learning model for the involved classification tasks. Justify your choice.
+# (b) Compare the accuracy of the 2 types of classification, i.e. intra-subject and cross subject data using your model. Explain your results.
+# (c) Explain the choices of hyper-parameters of your model architecture and analyze their influence on the results (for both 2 types of classification). How they are selected?
+# (d) If there is a significant difference in training and testing accuracies, what could be a possible reason? What are the alternative models or approaches you would select? Select one and implement to further improve your results. Justify your choice.
+
+
+
+
+
+
+
+
+
+
